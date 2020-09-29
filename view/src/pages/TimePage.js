7a56132ba1912544e52d854a7e8a1456b223d571
Avel111 <57135174+Avel111@users.noreply.github.com>
2020-09-29 22:53:40 +0300

    Add styles for timepage

--- view/src/pages/TimePage.js
+++ view/src/pages/TimePage.js
@@ -3,8 +3,13 @@ import React from 'react'
 
 export default function TimePage(){
     return(<>
-        <div>
-            <div>Заголовок</div>
+        <div className='wrapper'>
+            <div className='time-content content'>
+                <div className='time-block'>
+                    <div className='time-block__header'>Заголовок</div>
+                    <div className='time-block__timer'>Время</div>
+                </div>
+            </div>
         </div>
     </>)
 }
