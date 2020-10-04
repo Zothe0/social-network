80c41006f7dc2d310a2fe1177ccc9cd0d6661a25
Avel111 <57135174+Avel111@users.noreply.github.com>
2020-10-04 13:30:46 +0300

    Add styles for profilepage

--- view/src/pages/ProfilePage.js
+++ view/src/pages/ProfilePage.js
@@ -12,6 +12,10 @@ export default function ProfilePage(){
     const {id} = useParams()
     const [checkTokenExpire, logoutApp] = useCheckToken()
 
+    const formHandler = (e)=>{
+        e.prevenDefault()
+    }
+
     useEffect(()=>{
         checkTokenExpire()
     }, [checkTokenExpire])
@@ -19,5 +23,20 @@ export default function ProfilePage(){
     return(<>
         <title>Профиль</title>
         <Header/>
+        <div className='wrapper'>
+            <div className='profile'>
+                <div className='profile__container'>
+                    <div className='profile__header'>Загрузить фотографию</div>
+                    <form
+                        onSubmit={formHandler}
+                        className='profile__form'
+                        name='imageForm'
+                    >
+                        <input type='file' accept='image/jpeg,image/png'></input>
+                        <button type='submit'>Отправить</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     </>)
 }
